9b9a5673f1c1df579929303345b91ad8e4302cbf
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-08 22:21:23 +0100

    trie update

--- trie/node.go
+++ trie/node.go
@@ -17,3 +17,21 @@ func (node *Node[T]) walk(callback func(*Node[T])) {
 		}
 	}
 }
+
+func (node *Node[T]) Descend(str string) *Node[T] {
+	current := node
+
+	for _, char := range str {
+		if current.Children == nil {
+			return nil
+		}
+
+		if current.Children[char] == nil {
+			return nil
+		}
+
+		current = current.Children[char]
+	}
+
+	return current
+}
