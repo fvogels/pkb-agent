6da7bc99b0f32a4e9e9698f5925654fa897fdb77
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-08 22:19:50 +0100

    Graph.FindNameMatches

--- graph/graph.go
+++ graph/graph.go
@@ -28,3 +28,34 @@ func (graph *Graph) ListNodeNames() []string {
 
 	return result
 }
+
+func (graph *Graph) FindNameMatches(str string) MatchIterator {
+	trieNode := graph.trieRoot.Descend(str)
+
+	return MatchIterator{
+		current: trieNode,
+		index:   0,
+	}
+}
+
+type MatchIterator struct {
+	current *trie.Node[*Node]
+	index   int
+}
+
+func (iterator *MatchIterator) Current() *Node {
+	if iterator.current == nil {
+		return nil
+	}
+
+	return iterator.current.Terminals[iterator.index]
+}
+
+func (iterator *MatchIterator) Next() {
+	iterator.index++
+
+	if iterator.index == len(iterator.current.Terminals) {
+		iterator.current = iterator.current.NextTerminal
+		iterator.index = 0
+	}
+}
