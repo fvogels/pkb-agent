c915dce31964b650ce82b598df64dbdfc4b505a0
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-08 12:22:14 +0100

    graph loading update

--- graph/loader.go
+++ graph/loader.go
@@ -1,7 +1,7 @@
 package graph
 
 import (
-	"fmt"
+	"log/slog"
 	pathlib "pkb-agent/util/pathlib"
 )
 
@@ -9,14 +9,83 @@ type Loader interface {
 	Load(path pathlib.Path, callback func(node *Node) error) error
 }
 
+type GraphLoader struct {
+	root       pathlib.Path
+	nodeLoader Loader
+}
+
 func LoadGraph(root pathlib.Path, loader Loader) error {
-	callback := func(entry *Node) error {
-		fmt.Printf("%s\n", entry.Name)
+	graphLoader := GraphLoader{
+		root:       root,
+		nodeLoader: loader,
+	}
+	return graphLoader.Load()
+}
+
+func (gl *GraphLoader) Load() error {
+	nodes, err := gl.LoadNodes()
+	if err != nil {
+		return err
+	}
+
+	if err := gl.VerifyLinks(nodes); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type ErrNameClash struct{}
+
+func (err *ErrNameClash) Error() string {
+	return "name clash"
+}
+
+type ErrUnknownNodes struct{}
+
+func (err *ErrUnknownNodes) Error() string {
+	return "unknown node"
+}
+
+func (gl *GraphLoader) LoadNodes() (map[string]*Node, error) {
+	nodes := make(map[string]*Node)
+	duplicatesFound := false
+
+	callback := func(node *Node) error {
+		if _, alreadyExists := nodes[node.Name]; alreadyExists {
+			slog.Debug("Multiple nodes with same name", slog.String("name", node.Name))
+			duplicatesFound = true
+		}
+
+		nodes[node.Name] = node
 		return nil
 	}
 
-	if err := loader.Load(root, callback); err != nil {
-		return err
+	if err := gl.nodeLoader.Load(gl.root, callback); err != nil {
+		return nil, err
+	}
+
+	if duplicatesFound {
+		return nodes, &ErrNameClash{}
+	}
+
+	return nodes, nil
+}
+
+func (gl *GraphLoader) VerifyLinks(nodes map[string]*Node) error {
+	foundUnknownLinks := false
+
+	for _, node := range nodes {
+		for _, link := range node.Links {
+			if _, found := nodes[link]; !found {
+				slog.Debug("Unknown link", slog.String("node", node.Name), slog.String("link target", link))
+				foundUnknownLinks = true
+			}
+		}
+	}
+
+	if foundUnknownLinks {
+		return &ErrUnknownNodes{}
 	}
 
 	return nil
