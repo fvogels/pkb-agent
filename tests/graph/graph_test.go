7c5eb9052234db94ba25c0394c71366d172ed433
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-09 12:46:18 +0100

    graph tests

---
+++ tests/graph/graph_test.go
@@ -0,0 +1,54 @@
+//go:build test
+
+package graph
+
+import (
+	"pkb-agent/graph"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDescend(t *testing.T) {
+	t.Run("Matching", func(t *testing.T) {
+		builder := graph.NewBuilder()
+		builder.AddNode(&graph.Node{
+			Name:  "a",
+			Links: nil,
+		})
+		builder.AddNode(&graph.Node{
+			Name:  "b",
+			Links: nil,
+		})
+
+		g, err := builder.Finish()
+		require.NoError(t, err)
+
+		iterator := g.FindNameMatches("a")
+		require.NotNil(t, iterator.Current())
+		require.Equal(t, "a", iterator.Current().Name)
+		iterator.Next()
+		require.Nil(t, iterator.Current())
+	})
+
+	t.Run("Matching 2", func(t *testing.T) {
+		builder := graph.NewBuilder()
+		builder.AddNode(&graph.Node{
+			Name:  "aaa",
+			Links: nil,
+		})
+		builder.AddNode(&graph.Node{
+			Name:  "bbb",
+			Links: nil,
+		})
+
+		g, err := builder.Finish()
+		require.NoError(t, err)
+
+		iterator := g.FindNameMatches("a")
+		require.NotNil(t, iterator.Current())
+		require.Equal(t, "aaa", iterator.Current().Name)
+		iterator.Next()
+		require.Nil(t, iterator.Current())
+	})
+}
