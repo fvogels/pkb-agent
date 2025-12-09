fff4290d924d84717d4eb492fbe353bcc1b6f659
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-09 02:07:12 +0100

    trie tests

---
+++ tests/trie/trie_test.go
@@ -0,0 +1,45 @@
+//go:build test
+
+package trie
+
+import (
+	"pkb-agent/trie"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDescend(t *testing.T) {
+	t.Run("Single terminal", func(t *testing.T) {
+		builder := trie.NewBuilder[string]()
+		builder.Add("abc", "abc")
+		root := builder.Finish()
+
+		node := root.Descend("abc")
+
+		require.Len(t, node.Terminals, 1)
+		require.Equal(t, "abc", node.Terminals[0])
+		require.Nil(t, node.NextTerminal)
+	})
+
+	t.Run("Prefix", func(t *testing.T) {
+		builder := trie.NewBuilder[string]()
+		builder.Add("abc", "abc")
+		root := builder.Finish()
+
+		node := root.Descend("ab")
+
+		require.Len(t, node.Terminals, 0)
+		require.Equal(t, root.Descend("abc"), node.NextTerminal)
+	})
+
+	t.Run("Nonexistent prefix", func(t *testing.T) {
+		builder := trie.NewBuilder[string]()
+		builder.Add("abc", "abc")
+		root := builder.Finish()
+
+		node := root.Descend("x")
+
+		require.Nil(t, node)
+	})
+}
