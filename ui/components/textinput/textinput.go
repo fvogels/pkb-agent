a18d428a046ac65b1ad79c113fb73e8558d3aa27
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-09 23:57:44 +0100

    textinput styling and cursor

--- ui/components/textinput/textinput.go
+++ ui/components/textinput/textinput.go
@@ -1,19 +1,36 @@
 package textinput
 
 import (
+	"pkb-agent/ui/debug"
+	"pkb-agent/util"
+
+	"github.com/charmbracelet/bubbles/cursor"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Model struct {
-	input string
+	size   util.Size
+	input  string
+	style  lipgloss.Style
+	cursor cursor.Model
 }
 
 func New() Model {
-	return Model{}
+	c := cursor.New()
+	c.SetChar("█")
+
+	return Model{
+		style:  lipgloss.NewStyle().Background(lipgloss.Color("#5555FF")),
+		cursor: c,
+	}
 }
 
 func (model Model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(
+		model.cursor.Focus(),
+		model.cursor.SetMode(cursor.CursorStatic),
+	)
 }
 
 func (model Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
@@ -21,6 +38,8 @@ func (model Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (model Model) TypedUpdate(message tea.Msg) (Model, tea.Cmd) {
+	debug.ShowBubbleTeaMessage(message)
+
 	switch message := message.(type) {
 	case tea.KeyMsg:
 		return model.onKeyPressed(message)
@@ -29,13 +48,24 @@ func (model Model) TypedUpdate(message tea.Msg) (Model, tea.Cmd) {
 		model.input = ""
 		return model, model.signalUpdate()
 
+	case tea.WindowSizeMsg:
+		return model.onResize(message)
+
 	default:
-		return model, nil
+		updatedCursor, command := model.cursor.Update(message)
+		model.cursor = updatedCursor
+		return model, command
 	}
 }
 
 func (model Model) View() string {
-	return model.input
+	return model.style.Width(model.size.Width).Render(
+		lipgloss.JoinHorizontal(
+			0,
+			model.input,
+			model.cursor.View(),
+		),
+	)
 }
 
 func (model Model) onKeyPressed(message tea.KeyMsg) (Model, tea.Cmd) {
@@ -68,4 +98,13 @@ func (model *Model) signalUpdate() tea.Cmd {
 			Input: model.input,
 		}
 	}
+}
+
+func (model Model) onResize(message tea.WindowSizeMsg) (Model, tea.Cmd) {
+	model.size = util.Size{
+		Width:  message.Width,
+		Height: message.Height,
+	}
+
+	return model, nil
 }
