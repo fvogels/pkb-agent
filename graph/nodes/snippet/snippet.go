4f34c7dd27dc0686ff067771f0fb27d8b5fd327c
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-11 01:54:37 +0100

    snippet GetSource

--- graph/nodes/snippet/snippet.go
+++ graph/nodes/snippet/snippet.go
@@ -1,9 +1,36 @@
 package snippet
 
 import (
+	"os"
 	pathlib "pkb-agent/util/pathlib"
+	"strings"
 )
 
 type Extra struct {
 	Path pathlib.Path
+}
+
+func (data *Extra) GetSource() (string, error) {
+	buffer, err := os.ReadFile(data.Path.String())
+
+	if err != nil {
+		return "", err
+	}
+
+	lineGenerator := strings.Lines(string(buffer))
+	lines := []string{}
+	foundMetadataSeparator := false
+	lineGenerator(func(line string) bool {
+		if !foundMetadataSeparator {
+			if strings.TrimSpace(line) == "---" {
+				foundMetadataSeparator = true
+			}
+		} else {
+			lines = append(lines, line)
+		}
+
+		return true
+	})
+
+	return strings.Join(lines, ""), nil
 }
