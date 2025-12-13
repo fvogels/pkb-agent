2b7a13f3c2ab4d46c843ea3f2e744b1f26998ad2
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-13 17:38:03 +0100

    ability to open backblaze files

--- ui/nodeviewers/bbviewer/bbviewer.go
+++ ui/nodeviewers/bbviewer/bbviewer.go
@@ -5,9 +5,12 @@ import (
 	"log/slog"
 	"os"
 	bb "pkb-agent/backblaze"
+	"pkb-agent/extern"
 	"pkb-agent/graph/nodes/backblaze"
 	"pkb-agent/ui/debug"
 	"pkb-agent/util"
+	"pkb-agent/util/pathlib"
+	"pkb-agent/zipfile"
 
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
@@ -75,25 +78,45 @@ func (model Model) onKeyPressed(message tea.KeyMsg) (Model, tea.Cmd) {
 }
 
 func (model Model) openFile() (Model, tea.Cmd) {
-	application_key := os.Getenv("APPLICATION_KEY")
-	application_key_id := os.Getenv("APPLICATION_KEY_ID")
+	command := func() tea.Msg {
+		application_key := os.Getenv("APPLICATION_KEY")
+		application_key_id := os.Getenv("APPLICATION_KEY_ID")
+		ctx := context.Background()
 
-	ctx := context.Background()
+		client, err := bb.New(ctx, application_key, application_key_id)
+		if err != nil {
+			slog.Error("Failed to create backblaze client")
+			panic("failed to create backblaze client")
+		}
 
-	client, err := bb.New(ctx, application_key, application_key_id)
-	if err != nil {
-		slog.Error("Failed to create backblaze client")
-		panic("failed to create backblaze client")
+		buffer, err := client.DownloadToBuffer(ctx, model.nodeData.BucketName, model.nodeData.Filename, 1, func(n int) {})
+		if err != nil {
+			slog.Error(
+				"Failed to download file from BackBlaze",
+				slog.String("bucket", model.nodeData.BucketName),
+				slog.String("filename", model.nodeData.Filename),
+			)
+			panic("failed to download file")
+		}
+
+		zippedFiles, err := zipfile.Unpack(buffer)
+		if err != nil {
+			slog.Error("Failed to unzip files")
+			panic("failed to unzip files")
+		}
+
+		zippedFile := zippedFiles[0]
+		path, err := zippedFile.SaveToDirectory(pathlib.New("."))
+		if err != nil {
+			return err
+		}
+
+		if err := extern.OpenUsingDefaultViewer(path); err != nil {
+			return err
+		}
+
+		return nil
 	}
 
-	if err := client.DownloadToFile(ctx, model.nodeData.BucketName, model.nodeData.Filename, model.nodeData.Filename, 1, func(n int) {}); err != nil {
-		slog.Error(
-			"Failed to download file from BackBlaze",
-			slog.String("bucket", model.nodeData.BucketName),
-			slog.String("filename", model.nodeData.Filename),
-		)
-		panic("failed to download file")
-	}
-
-	return model, nil
+	return model, command
 }
