2468e42fbebbc52dcf95da59263beffae71039d5
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-18 17:08:41 +0100

    removed redundant code

--- ui/screens/mainscreen/viewmode.go
+++ ui/screens/mainscreen/viewmode.go
@@ -113,9 +113,6 @@ func (mode viewMode) onKeyPressed(model Model, message tea.KeyMsg) (Model, tea.C
 		return model, tea.Quit
 
 	case key.Matches(message, viewModeKeyMap.SwitchToInputMode):
-		// model.mode = model.inputMode
-		// command := model.mode.activate(&model)
-		// return model, command
 		return model, func() tea.Msg { return msgSwitchMode{mode: model.inputMode} }
 
 	case key.Matches(message, viewModeKeyMap.HighlightFirst):
