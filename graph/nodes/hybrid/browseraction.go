4a3ec44a94086de423e8bcab412b4883afb25bfc
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-28 02:01:05 +0100

    fixed bug

--- graph/nodes/hybrid/browseraction.go
+++ graph/nodes/hybrid/browseraction.go
@@ -10,12 +10,12 @@ type BrowserAction struct {
 func parseBrowserAction(metadata map[string]string) (*BrowserAction, error) {
 	description, found := metadata["description"]
 	if !found {
-		return nil, fmt.Errorf("missing description: %w", ErrInvalidAction{})
+		return nil, fmt.Errorf("missing description: %w", &ErrInvalidAction{})
 	}
 
 	url, found := metadata["url"]
 	if !found {
-		return nil, fmt.Errorf("missing url: %w", ErrInvalidAction{})
+		return nil, fmt.Errorf("missing url: %w", &ErrInvalidAction{})
 	}
 
 	action := BrowserAction{
