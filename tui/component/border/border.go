ad341995752aca3e3dfb29d736feda22988d420b
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-06 19:24:46 +0100

    border component

---
+++ tui/component/border/border.go
@@ -0,0 +1,44 @@
+package border
+
+import (
+	"pkb-agent/tui"
+)
+
+type Component struct {
+	size  tui.Size
+	child tui.Component
+	style tui.Style
+}
+
+func New(child tui.Component, style tui.Style) *Component {
+	return &Component{
+		child: child,
+		style: style,
+	}
+}
+
+func (component *Component) Handle(message tui.Message) {
+	switch message := message.(type) {
+	case tui.MsgResize:
+		component.onResize(message)
+	}
+}
+
+func (component *Component) Render() tui.Grid {
+	return &grid{
+		childGrid: component.child.Render(),
+		style:     component.style,
+	}
+}
+
+func (component *Component) onResize(message tui.MsgResize) {
+	component.size = message.Size
+
+	childSizeMessage := tui.MsgResize{
+		Size: tui.Size{
+			Width:  message.Size.Width - 2,
+			Height: component.size.Height - 2,
+		},
+	}
+	component.child.Handle(childSizeMessage)
+}
---
+++ tui/component/border/grid.go
@@ -0,0 +1,84 @@
+package border
+
+import (
+	"pkb-agent/tui"
+
+	"github.com/gdamore/tcell/v3"
+)
+
+type grid struct {
+	childGrid tui.Grid
+	style     tui.Style
+}
+
+func (grid *grid) GetSize() tui.Size {
+	childSize := grid.childGrid.GetSize()
+
+	return tui.Size{
+		Width:  childSize.Width + 2,
+		Height: childSize.Height + 2,
+	}
+}
+
+func (grid *grid) Get(position tui.Position) tui.Cell {
+	if tui.SafeMode && !grid.isValidPosition(position) {
+		panic("invalid coordinates")
+	}
+
+	x := position.X
+	y := position.Y
+	size := grid.GetSize()
+	width := size.Width
+	height := size.Height
+
+	var char rune
+	var style *tui.Style
+
+	if x == 0 {
+		style = &grid.style
+
+		if y == 0 {
+			// Upper left corner
+			char = tcell.RuneULCorner
+		} else if y == height-1 {
+			// Lower left corner
+			char = tcell.RuneLLCorner
+		} else {
+			// Left border
+			char = tcell.RuneVLine
+		}
+	} else if x == width-1 {
+		style = &grid.style
+
+		if y == 0 {
+			// Upper right corner
+			char = tcell.RuneURCorner
+		} else if y == height-1 {
+			// Lower right corner
+			char = tcell.RuneLRCorner
+		} else {
+			// Right border
+			char = tcell.RuneVLine
+		}
+	} else if y == 0 || y == height-1 {
+		style = &grid.style
+		char = tcell.RuneHLine
+	} else {
+		cell := grid.childGrid.Get(tui.Position{X: x - 1, Y: y - 1})
+		style = cell.Style
+		char = cell.Contents
+	}
+
+	return tui.Cell{
+		Contents: char,
+		Style:    style,
+	}
+}
+
+func (grid *grid) isValidPosition(position tui.Position) bool {
+	x := position.X
+	y := position.Y
+	size := grid.GetSize()
+
+	return 0 <= x && x < size.Width && 0 <= y && y < size.Height
+}
