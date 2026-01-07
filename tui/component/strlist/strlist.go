45ff8b7d5c6bf89e7b5a8b335cb9cda811cb1781
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-07 23:35:32 +0100

    strlist

---
+++ tui/component/strlist/strlist.go
@@ -0,0 +1,58 @@
+package strlist
+
+import (
+	"pkb-agent/tui"
+	"pkb-agent/tui/data"
+)
+
+type Component struct {
+	size              tui.Size
+	items             data.List[string]
+	selectedIndex     data.Value[int]
+	emptyStyle        *tui.Style
+	itemStyle         *tui.Style
+	selectedStyle     *tui.Style
+	firstVisibleIndex int
+}
+
+func New(items data.List[string], selectedItem data.Value[int], style *tui.Style, selectedStyle *tui.Style) *Component {
+	return &Component{
+		items:             items,
+		selectedIndex:     selectedItem,
+		itemStyle:         style,
+		emptyStyle:        style,
+		selectedStyle:     selectedStyle,
+		firstVisibleIndex: 0,
+	}
+}
+
+func (component *Component) Handle(message tui.Message) {
+	switch message := message.(type) {
+	case tui.MsgResize:
+		component.onResize(message)
+	}
+}
+
+func (component *Component) Render() tui.Grid {
+	return NewGrid(component)
+}
+
+func (component *Component) onResize(message tui.MsgResize) {
+	component.size = message.Size
+
+	component.ensureSelectedItemIsVisible()
+}
+
+func (component *Component) ensureSelectedItemIsVisible() {
+	selectedIndex := component.selectedIndex.Get()
+
+	if component.items.Size() == 0 {
+		component.firstVisibleIndex = 0
+	} else {
+		if component.selectedIndex.Get() < component.firstVisibleIndex {
+			component.firstVisibleIndex = selectedIndex
+		} else if component.firstVisibleIndex+component.size.Height <= selectedIndex {
+			component.firstVisibleIndex = selectedIndex - component.size.Height + 1
+		}
+	}
+}
