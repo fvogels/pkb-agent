232a77da6deddf7be593fd23d9c9e3121a5a6eb6
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-07 10:37:03 +0100

    variable is settable

--- tui/data/variable.go
+++ tui/data/variable.go
@@ -1,15 +1,19 @@
 package data
 
-type variable[T any] struct {
+type Variable[T any] struct {
 	value T
 }
 
-func NewVariable[T any](value T) Data[T] {
-	result := variable[T]{value: value}
+func NewVariable[T any](value T) *Variable[T] {
+	result := Variable[T]{value: value}
 
 	return &result
 }
 
-func (v *variable[T]) Get() T {
+func (v *Variable[T]) Set(value T) {
+	v.value = value
+}
+
+func (v *Variable[T]) Get() T {
 	return v.value
 }
