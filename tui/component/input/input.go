c60bd4322f647fd2c04dad1c2d09510044fe8777
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-09 23:05:08 +0100

    updated input component

--- tui/component/input/input.go
+++ tui/component/input/input.go
@@ -4,23 +4,35 @@ import (
 	"pkb-agent/tui"
 	"pkb-agent/tui/component/label"
 	"pkb-agent/tui/data"
+
+	"github.com/gdamore/tcell/v3"
 )
 
 type Component struct {
 	size     tui.Size
 	contents data.Value[string]
-	style    tui.Style
+	style    *tui.Style
 	onChange func(string)
 	label    *label.Component
 }
 
-func New(contents data.Value[string], style tui.Style, onChange func(string)) *Component {
+func New(contents data.Value[string]) *Component {
+	style := tcell.StyleDefault
+
 	return &Component{
 		contents: contents,
-		style:    style,
-		onChange: onChange,
+		style:    &style,
+		onChange: nil,
 		label:    label.New(contents, style),
 	}
+}
+
+func (component *Component) SetStyle(style *tui.Style) {
+	component.style = style
+}
+
+func (component *Component) SetOnChange(callback func(string)) {
+	component.onChange = callback
 }
 
 func (component *Component) Handle(message tui.Message) {
@@ -46,12 +58,18 @@ func (component *Component) onResize(message tui.MsgResize) {
 func (component *Component) onKey(message tui.MsgKey) {
 	if len(message.Key) == 1 {
 		updatedContents := component.contents.Get() + message.Key
-		component.onChange(updatedContents)
+		component.signalOnChange(updatedContents)
 	} else if message.Key == "Backspace" {
 		currentContents := component.contents.Get()
 
 		if len(currentContents) > 0 {
-			component.onChange(currentContents[:len(currentContents)-1])
+			component.signalOnChange(currentContents[:len(currentContents)-1])
 		}
+	}
+}
+
+func (component *Component) signalOnChange(contents string) {
+	if component.onChange != nil {
+		component.onChange(contents)
 	}
 }
