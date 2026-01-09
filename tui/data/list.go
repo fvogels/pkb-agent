5c1643653b9a416deee4e8ebe524e879ccefd48b
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-09 18:44:22 +0100

    CopyListToSlice

--- tui/data/list.go
+++ tui/data/list.go
@@ -5,3 +5,13 @@ type List[T any] interface {
 	At(index int) T
 	Observe(func())
 }
+
+func CopyListToSlice[T any](list List[T]) []T {
+	result := make([]T, list.Size())
+
+	for i := range list.Size() {
+		result[i] = list.At(i)
+	}
+
+	return result
+}
