24f0b4527544c853d2521e4a4e095ed02e935a06
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-09 14:29:31 +0100

    MemoizeValue

---
+++ tui/data/memovalue.go
@@ -0,0 +1,29 @@
+package data
+
+func MemoizeValue[T any](value Value[T]) Value[T] {
+	result := memoizedValue[T]{
+		original: value,
+		cached:   value.Get(),
+	}
+
+	value.Observe(func() { result.refresh() })
+
+	return &result
+}
+
+type memoizedValue[T any] struct {
+	original Value[T]
+	cached   T
+}
+
+func (value *memoizedValue[T]) Get() T {
+	return value.cached
+}
+
+func (value *memoizedValue[T]) refresh() {
+	value.cached = value.original.Get()
+}
+
+func (value *memoizedValue[T]) Observe(observer func()) {
+	value.original.Observe(observer)
+}
