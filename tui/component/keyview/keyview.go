55a25621e306a5732bd5cb947b9b65d767f4abc6
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-14 23:20:16 +0100

    keyview component

---
+++ tui/component/keyview/keyview.go
@@ -0,0 +1,99 @@
+package keyview
+
+import (
+	"pkb-agent/persistent/list"
+	"pkb-agent/tui"
+	"pkb-agent/tui/data"
+
+	"github.com/gdamore/tcell/v3"
+	"github.com/gdamore/tcell/v3/color"
+)
+
+type Component struct {
+	tui.ComponentBase
+	size             tui.Size
+	keyBindings      data.Value[list.List[KeyBinding]]
+	cachedGrid       tui.Grid
+	keyStyle         *tui.Style
+	descriptionStyle *tui.Style
+	emptyStyle       *tui.Style
+}
+
+type KeyBinding struct {
+	key         string
+	description string
+}
+
+func New(messageQueue tui.MessageQueue, name string, keyBindings data.Value[list.List[KeyBinding]]) *Component {
+	keyStyle := tcell.StyleDefault.Background(color.NewHexColor(0xAAAAFF))
+	descriptionStyle := tcell.StyleDefault.Background(color.NewHexColor(0x8888FF))
+	emptyStyle := tcell.StyleDefault.Background(color.Reset)
+
+	component := Component{
+		ComponentBase: tui.ComponentBase{
+			Name:         name,
+			MessageQueue: messageQueue,
+		},
+		keyBindings:      keyBindings,
+		cachedGrid:       nil,
+		keyStyle:         &keyStyle,
+		descriptionStyle: &descriptionStyle,
+		emptyStyle:       &emptyStyle,
+	}
+
+	keyBindings.Observe(func() { component.cachedGrid = nil })
+
+	return &component
+}
+
+func (component *Component) Handle(message tui.Message) {
+	switch message := message.(type) {
+	case tui.MsgResize:
+		component.onResize(message)
+	}
+}
+
+func (component *Component) Render() tui.Grid {
+	if component.cachedGrid == nil {
+		component.cachedGrid = component.renderKeyBindings()
+	}
+
+	return component.cachedGrid
+}
+
+func (component *Component) onResize(message tui.MsgResize) {
+	component.size = message.Size
+	component.cachedGrid = nil
+}
+
+func (component *Component) renderKeyBindings() tui.Grid {
+	cell := tui.Cell{
+		Contents: ' ',
+		Style:    component.emptyStyle,
+	}
+	result := tui.NewMaterializedGrid(component.size, func(tui.Position) tui.Cell { return cell })
+
+	x := 0
+	write := func(contents rune, style *tui.Style) {
+		if x < result.GetSize().Width {
+			result.Set(tui.Position{X: x, Y: 0}, tui.Cell{Contents: contents, Style: style})
+			x++
+		}
+	}
+	list.ForEach(component.keyBindings.Get(), func(index int, keyBinding KeyBinding) {
+		write(' ', component.keyStyle)
+		for _, r := range []rune(keyBinding.key) {
+			write(r, component.keyStyle)
+		}
+		write(' ', component.keyStyle)
+
+		write(' ', component.descriptionStyle)
+		for _, r := range []rune(keyBinding.description) {
+			write(r, component.descriptionStyle)
+		}
+		write(' ', component.descriptionStyle)
+		write(' ', component.emptyStyle)
+	})
+
+	return result
+}
