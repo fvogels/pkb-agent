2081d8a052346568faf3fb81a3328f72e87bb373
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-14 14:19:25 +0100

    value mapping made lazy

--- tui/data/mapvalue.go
+++ tui/data/mapvalue.go
@@ -1,40 +1,59 @@
 package data
 
+type mappedValue[T any, U any] struct {
+	originalValue          Value[T]
+	transformer            func(T) U
+	dirty                  bool
+	cachedTransformedValue U
+}
+
 func MapValue[T any, U any](value Value[T], transformer func(T) U) Value[U] {
 	result := mappedValue[T, U]{
-		originalValue:    value,
-		transformedValue: transformer(value.Get()),
+		originalValue:          value,
+		transformer:            transformer,
+		cachedTransformedValue: transformer(value.Get()),
+		dirty:                  false,
 	}
 
 	value.Observe(func() {
-		result.transformedValue = transformer(value.Get())
+		result.dirty = true
 	})
 
 	return &result
 }
 
-type mappedValue[T any, U any] struct {
-	originalValue    Value[T]
-	transformedValue U
-}
-
 func (value *mappedValue[T, U]) Get() U {
-	return value.transformedValue
+	if value.dirty {
+		value.cachedTransformedValue = value.transformer(value.originalValue.Get())
+		value.dirty = false
+	}
+
+	return value.cachedTransformedValue
 }
 
 func (value *mappedValue[T, U]) Observe(observer func()) {
 	value.originalValue.Observe(observer)
 }
 
+type mappedValue2[T1, T2, R any] struct {
+	originalValue1   Value[T1]
+	originalValue2   Value[T2]
+	transformer      func(T1, T2) R
+	transformedValue R
+	dirty            bool
+}
+
 func MapValue2[T1, T2, R any](value1 Value[T1], value2 Value[T2], transformer func(T1, T2) R) Value[R] {
 	result := mappedValue2[T1, T2, R]{
 		originalValue1:   value1,
 		originalValue2:   value2,
+		transformer:      transformer,
 		transformedValue: transformer(value1.Get(), value2.Get()),
+		dirty:            false,
 	}
 
 	observer := func() {
-		result.transformedValue = transformer(value1.Get(), value2.Get())
+		result.dirty = true
 	}
 
 	value1.Observe(observer)
@@ -43,13 +62,11 @@ func MapValue2[T1, T2, R any](value1 Value[T1], value2 Value[T2], transformer fu
 	return &result
 }
 
-type mappedValue2[T1, T2, R any] struct {
-	originalValue1   Value[T1]
-	originalValue2   Value[T2]
-	transformedValue R
-}
-
 func (value *mappedValue2[T1, T2, R]) Get() R {
+	if value.dirty {
+		value.transformedValue = value.transformer(value.originalValue1.Get(), value.originalValue2.Get())
+		value.dirty = false
+	}
 	return value.transformedValue
 }
 
@@ -58,16 +75,27 @@ func (value *mappedValue2[T1, T2, R]) Observe(observer func()) {
 	value.originalValue2.Observe(observer)
 }
 
+type mappedValue3[T1, T2, T3, R any] struct {
+	originalValue1   Value[T1]
+	originalValue2   Value[T2]
+	originalValue3   Value[T3]
+	transformer      func(T1, T2, T3) R
+	transformedValue R
+	dirty            bool
+}
+
 func MapValue3[T1, T2, T3, R any](value1 Value[T1], value2 Value[T2], value3 Value[T3], transformer func(T1, T2, T3) R) Value[R] {
 	result := mappedValue3[T1, T2, T3, R]{
 		originalValue1:   value1,
 		originalValue2:   value2,
 		originalValue3:   value3,
+		transformer:      transformer,
 		transformedValue: transformer(value1.Get(), value2.Get(), value3.Get()),
+		dirty:            false,
 	}
 
 	observer := func() {
-		result.transformedValue = transformer(value1.Get(), value2.Get(), value3.Get())
+		result.dirty = true
 	}
 
 	value1.Observe(observer)
@@ -77,14 +105,12 @@ func MapValue3[T1, T2, T3, R any](value1 Value[T1], value2 Value[T2], value3 Val
 	return &result
 }
 
-type mappedValue3[T1, T2, T3, R any] struct {
-	originalValue1   Value[T1]
-	originalValue2   Value[T2]
-	originalValue3   Value[T3]
-	transformedValue R
-}
-
 func (value *mappedValue3[T1, T2, T3, R]) Get() R {
+	if value.dirty {
+		value.transformedValue = value.transformer(value.originalValue1.Get(), value.originalValue2.Get(), value.originalValue3.Get())
+		value.dirty = false
+	}
+
 	return value.transformedValue
 }
 
