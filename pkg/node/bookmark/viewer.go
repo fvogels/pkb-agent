89d4d5a23ec47d1bd24e78dd1f8dfaf339ba2acb
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-15 16:52:25 +0100

    bookmark viewer

--- pkg/node/bookmark/viewer.go
+++ pkg/node/bookmark/viewer.go
@@ -1,92 +1,135 @@
 package bookmark
 
 import (
-	"pkb-agent/extern"
-	"pkb-agent/pkg/node"
-	"pkb-agent/util"
-
-	"github.com/charmbracelet/bubbles/key"
-	tea "github.com/charmbracelet/bubbletea"
+	"pkb-agent/persistent/list"
+	"pkb-agent/tui"
+	"pkb-agent/tui/application/messages"
+	"pkb-agent/tui/component/label"
+	"pkb-agent/tui/data"
+	"pkb-agent/ui/uid"
 )
 
-var keyMap = struct {
-	OpenInBrowser key.Binding
-}{
-	OpenInBrowser: key.NewBinding(
-		key.WithKeys("o"),
-		key.WithHelp("o", "open"),
-	),
+type Component struct {
+	tui.ComponentBase
+	child tui.Component
 }
 
-type Model struct {
-	size util.Size
-	url  string
-}
-
-func NewViewer(url string) Model {
-	return Model{
-		url: url,
+func NewViewer(messageQueue tui.MessageQueue) *Component {
+	component := Component{
+		ComponentBase: tui.ComponentBase{
+			Identifier:   uid.Generate(),
+			Name:         "unnamed bookmark viewer",
+			MessageQueue: messageQueue,
+		},
+		child: label.New(messageQueue, "bookmark label", data.NewConstant("bookmark!")),
 	}
+
+	return &component
 }
 
-func (model Model) Init() tea.Cmd {
-	return model.signalKeybindingsUpdate()
-}
-
-func (model Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
-	return model.TypedUpdate(message)
-}
-
-func (model Model) TypedUpdate(message tea.Msg) (Model, tea.Cmd) {
+func (component *Component) Handle(message tui.Message) {
 	switch message := message.(type) {
-	case tea.WindowSizeMsg:
-		return model.onResize(message)
+	case tui.MsgActivate:
+		component.onActivate()
 
-	case tea.KeyMsg:
-		return model.onKeyPressed(message)
-
-	default:
-		return model, nil
+	case tui.MsgResize:
+		component.onResize(message)
 	}
 }
 
-func (model Model) onResize(message tea.WindowSizeMsg) (Model, tea.Cmd) {
-	model.size = util.Size{
-		Width:  message.Width,
-		Height: message.Height,
-	}
-
-	return model, nil
+func (component *Component) onActivate() {
+	component.MessageQueue.Enqueue(messages.MsgSetNodeKeyBindings{
+		Bindings: list.New[tui.KeyBinding](),
+	})
 }
 
-func (model Model) onKeyPressed(message tea.KeyMsg) (Model, tea.Cmd) {
-	switch {
-	case key.Matches(message, keyMap.OpenInBrowser):
-		return model.onOpenInBrowser()
-
-	default:
-		return model, nil
-	}
+func (component *Component) Render() tui.Grid {
+	return component.child.Render()
 }
 
-func (model Model) onOpenInBrowser() (Model, tea.Cmd) {
-	if err := extern.OpenURLInBrowser(model.url); err != nil {
-		panic("failed to open browser")
-	}
-
-	return model, nil
+func (component *Component) onResize(message tui.MsgResize) {
+	component.Size = message.Size
+	component.child.Handle(message)
 }
 
-func (model Model) View() string {
-	return model.url
-}
+// var keyMap = struct {
+// 	OpenInBrowser key.Binding
+// }{
+// 	OpenInBrowser: key.NewBinding(
+// 		key.WithKeys("o"),
+// 		key.WithHelp("o", "open"),
+// 	),
+// }
 
-func (model Model) signalKeybindingsUpdate() tea.Cmd {
-	return func() tea.Msg {
-		return node.MsgUpdateNodeViewerBindings{
-			KeyBindings: []key.Binding{
-				keyMap.OpenInBrowser,
-			},
-		}
-	}
-}
+// type Model struct {
+// 	size util.Size
+// 	url  string
+// }
+
+// func NewViewer(url string) Model {
+// 	return Model{
+// 		url: url,
+// 	}
+// }
+
+// func (model Model) Init() tea.Cmd {
+// 	return model.signalKeybindingsUpdate()
+// }
+
+// func (model Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+// 	return model.TypedUpdate(message)
+// }
+
+// func (model Model) TypedUpdate(message tea.Msg) (Model, tea.Cmd) {
+// 	switch message := message.(type) {
+// 	case tea.WindowSizeMsg:
+// 		return model.onResize(message)
+
+// 	case tea.KeyMsg:
+// 		return model.onKeyPressed(message)
+
+// 	default:
+// 		return model, nil
+// 	}
+// }
+
+// func (model Model) onResize(message tea.WindowSizeMsg) (Model, tea.Cmd) {
+// 	model.size = util.Size{
+// 		Width:  message.Width,
+// 		Height: message.Height,
+// 	}
+
+// 	return model, nil
+// }
+
+// func (model Model) onKeyPressed(message tea.KeyMsg) (Model, tea.Cmd) {
+// 	switch {
+// 	case key.Matches(message, keyMap.OpenInBrowser):
+// 		return model.onOpenInBrowser()
+
+// 	default:
+// 		return model, nil
+// 	}
+// }
+
+// func (model Model) onOpenInBrowser() (Model, tea.Cmd) {
+// 	if err := extern.OpenURLInBrowser(model.url); err != nil {
+// 		panic("failed to open browser")
+// 	}
+
+// 	return model, nil
+// }
+
+// func (model Model) View() string {
+// 	return model.url
+// }
+
+// func (model Model) signalKeybindingsUpdate() tea.Cmd {
+// 	return func() tea.Msg {
+// 		return node.MsgUpdateNodeViewerBindings{
+// 			KeyBindings: []key.Binding{
+// 				keyMap.OpenInBrowser,
+// 			},
+// 		}
+// 	}
+// }
