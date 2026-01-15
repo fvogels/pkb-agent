e6aa99c9eb6afea9a86c220d2104b81578ad744e
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-15 18:40:35 +0100

    refactoring

--- tests/testlib/node.go
+++ tests/testlib/node.go
@@ -23,7 +23,7 @@ func (node *TestNode) GetLinks() []string {
 	return node.Links
 }
 
-func (node *TestNode) GetViewer(tui.MessageQueue) tui.Component {
+func (node *TestNode) CreateViewer(tui.MessageQueue) tui.Component {
 	return nil
 }
 
