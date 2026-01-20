d815b84ad6555fad041e695f6b7b3e70b1e2d514
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-20 17:35:01 +0100

    captioned is measurable

--- tui/component/captioned/captioned.go
+++ tui/component/captioned/captioned.go
@@ -1,9 +1,11 @@
 package captioned
 
 import (
+	"fmt"
 	"pkb-agent/tui"
 	"pkb-agent/tui/data"
 	"pkb-agent/util/uid"
+	"reflect"
 
 	"github.com/gdamore/tcell/v3"
 	"github.com/gdamore/tcell/v3/color"
@@ -16,21 +18,35 @@ type Component struct {
 	child        tui.Component
 }
 
-func New(messageQueue *tui.MessageQueue, caption data.Value[[]rune], child tui.Component) *Component {
-	captionStyle := tcell.StyleDefault.Background(color.Blue).Foreground(color.Reset)
+type MeasurableComponent struct {
+	Component
+}
 
-	component := Component{
-		ComponentBase: tui.ComponentBase{
-			Identifier:   uid.Generate(),
-			Name:         "unnamed captioned",
-			MessageQueue: *messageQueue,
-		},
-		caption:      caption,
-		captionStyle: &captionStyle,
-		child:        child,
-	}
+func New(messageQueue tui.MessageQueue, caption data.Value[[]rune], child tui.Component) *Component {
+	var component Component
+
+	initialize(&component, messageQueue, caption, child)
 
 	return &component
+}
+
+func initialize(component *Component, messageQueue tui.MessageQueue, caption data.Value[[]rune], child tui.Component) {
+	captionStyle := tcell.StyleDefault.Background(color.Blue).Foreground(color.Reset)
+
+	component.Identifier = uid.Generate()
+	component.Name = "unnamed captioned"
+	component.MessageQueue = messageQueue
+	component.caption = caption
+	component.captionStyle = &captionStyle
+	component.child = child
+}
+
+func NewMeasurable(messageQueue tui.MessageQueue, caption data.Value[[]rune], child tui.MeasurableComponent) *MeasurableComponent {
+	component := New(messageQueue, caption, child)
+
+	return &MeasurableComponent{
+		Component: *component,
+	}
 }
 
 func (component *Component) SetCaptionStyle(style *tui.Style) {
@@ -61,4 +77,19 @@ func (component *Component) onResize(message tui.MsgResize) {
 		},
 	}
 	component.child.Handle(childSizeMessage)
+}
+
+func (component *MeasurableComponent) Measure() tui.Size {
+	child := component.child
+	measurableChild, ok := child.(tui.MeasurableComponent)
+	if !ok {
+		panic(fmt.Sprintf("child with type %s is not measurable", reflect.TypeOf(component.child).String()))
+	}
+
+	childSize := measurableChild.Measure()
+
+	return tui.Size{
+		Width:  childSize.Width,
+		Height: childSize.Height + 1,
+	}
 }
