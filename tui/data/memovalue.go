55f2a55a4f5e29fd3ca01e21006dd5590be33314
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-21 18:50:18 +0100

    memovalue update

--- tui/data/memovalue.go
+++ tui/data/memovalue.go
@@ -1,27 +1,32 @@
 package data
 
 type memoizedValue[T any] struct {
-	original Value[T]
-	cached   T
+	original      Value[T]
+	cached        T
+	cachedVersion uint
 }
 
 func MemoizeValue[T any](value Value[T]) Value[T] {
 	result := memoizedValue[T]{
-		original: value,
-		cached:   value.Get(),
+		original:      value,
+		cached:        value.Get(),
+		cachedVersion: value.Version(),
 	}
-
-	value.Observe(func() { result.refresh() })
 
 	return &result
 }
 
 func (value *memoizedValue[T]) Get() T {
+	if value.cachedVersion != value.original.Version() {
+		value.refresh()
+	}
+
 	return value.cached
 }
 
 func (value *memoizedValue[T]) refresh() {
 	value.cached = value.original.Get()
+	value.cachedVersion = value.original.Version()
 }
 
 func (value *memoizedValue[T]) Observe(observer func()) {
