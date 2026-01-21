459e583a90b59f9a009aebc5bdcfa8494195e9df
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-21 23:22:54 +0100

    fixed atom viewer resize bug

--- pkg/node/atom/viewer.go
+++ pkg/node/atom/viewer.go
@@ -1,11 +1,14 @@
 package atom
 
 import (
+	"fmt"
+	"log/slog"
 	"pkb-agent/persistent/list"
 	"pkb-agent/tui"
 	"pkb-agent/tui/application/messages"
 	"pkb-agent/tui/component/label"
 	"pkb-agent/tui/data"
+	"pkb-agent/tui/debug"
 	"pkb-agent/util/uid"
 )
 
@@ -28,10 +31,15 @@ func NewViewer(messageQueue tui.MessageQueue) *Component {
 }
 
 func (component *Component) Handle(message tui.Message) {
+	debug.LogMessage(message)
+
 	switch message := message.(type) {
 	case tui.MsgStateUpdated:
 		component.child.Handle(message)
 		component.onStateUpdated()
+
+	case tui.MsgResize:
+		component.onResize(message)
 
 	default:
 		component.child.Handle(message)
@@ -45,10 +53,22 @@ func (component *Component) onStateUpdated() {
 }
 
 func (component *Component) Render() tui.Grid {
+	slog.Debug(
+		"Rendering atom view",
+		slog.String("size", component.Size.String()),
+		slog.String("address", fmt.Sprintf("%p", component)),
+	)
+
 	return component.child.Render()
 }
 
 func (component *Component) onResize(message tui.MsgResize) {
+	slog.Debug(
+		"Resizing atom viewer",
+		slog.String("size", message.Size.String()),
+		slog.String("address", fmt.Sprintf("%p", component)),
+	)
+
 	component.Size = message.Size
 	component.child.Handle(message)
 }
