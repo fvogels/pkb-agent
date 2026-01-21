adb4a24b0bd67e8de9d7c498b75768276a302826
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-21 23:48:41 +0100

    cleanup

--- tui/data/mapvalue.go
+++ tui/data/mapvalue.go
@@ -56,48 +56,31 @@ func (value *mappedValue2[T1, T2, R]) Version() uint {
 }
 
 type mappedValue3[T1, T2, T3, R any] struct {
-	argument1        Value[T1]
-	argument2        Value[T2]
-	argument3        Value[T3]
-	transformer      func(T1, T2, T3) R
-	transformedValue R
-	dirty            bool
+	argument1   Value[T1]
+	argument2   Value[T2]
+	argument3   Value[T3]
+	transformer func(T1, T2, T3) R
 }
 
 func MapValue3[T1, T2, T3, R any](value1 Value[T1], value2 Value[T2], value3 Value[T3], transformer func(T1, T2, T3) R) Value[R] {
 	result := mappedValue3[T1, T2, T3, R]{
-		argument1:        value1,
-		argument2:        value2,
-		argument3:        value3,
-		transformer:      transformer,
-		transformedValue: transformer(value1.Get(), value2.Get(), value3.Get()),
-		dirty:            false,
+		argument1:   value1,
+		argument2:   value2,
+		argument3:   value3,
+		transformer: transformer,
 	}
-
-	observer := func() {
-		result.dirty = true
-	}
-
-	value1.Observe(observer)
-	value2.Observe(observer)
-	value3.Observe(observer)
 
 	return &result
 }
 
 func (value *mappedValue3[T1, T2, T3, R]) Get() R {
-	if value.dirty {
-		value.transformedValue = value.transformer(value.argument1.Get(), value.argument2.Get(), value.argument3.Get())
-		value.dirty = false
-	}
-
-	return value.transformedValue
+	return value.transformer(value.argument1.Get(), value.argument2.Get(), value.argument3.Get())
 }
 
 func (value *mappedValue3[T1, T2, T3, R]) Observe(observer func()) {
-	value.argument1.Observe(observer)
-	value.argument2.Observe(observer)
-	value.argument3.Observe(observer)
+	// value.argument1.Observe(observer)
+	// value.argument2.Observe(observer)
+	// value.argument3.Observe(observer)
 }
 
 func (value *mappedValue3[T1, T2, T3, R]) Version() uint {
