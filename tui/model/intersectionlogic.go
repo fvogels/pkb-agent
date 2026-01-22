5c9b22b0fa48323442f5c70f51a613a1dce5d82a
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-22 18:10:21 +0100

    support for multiple search keywords

--- tui/model/intersectionlogic.go
+++ tui/model/intersectionlogic.go
@@ -4,6 +4,7 @@ import (
 	"pkb-agent/persistent/list"
 	"pkb-agent/pkg"
 	"pkb-agent/util"
+	"pkb-agent/util/queue"
 	"pkb-agent/util/set"
 	"slices"
 	"sort"
@@ -12,6 +13,28 @@ import (
 
 // determineIntersectionNodes computes which nodes are compatible with the selected nodes and the search filter.
 func determineIntersectionNodes(input string, graph *pkg.Graph, selectedNodes list.List[*pkg.Node], includeLinked bool, includeIndirectAncestors bool) list.List[*pkg.Node] {
+	r1 := collectDescendantIntersection(graph, selectedNodes, includeIndirectAncestors)
+
+	if includeLinked {
+		r1 = collectAncestorsUnion(graph, r1)
+	}
+
+	searchStrings := strings.Split(input, " ")
+	r2 := collectNodesMatchingSearches(graph, searchStrings)
+	r1.IntersectWith(r2)
+
+	result := []*pkg.Node{}
+	for i := range graph.GetNodeCount() {
+		if r1.Contains(i) {
+			node := graph.FindNodeByIndex(i)
+			result = append(result, node)
+		}
+	}
+
+	return list.FromSlice(result)
+}
+
+func determineIntersectionNodesOld(input string, graph *pkg.Graph, selectedNodes list.List[*pkg.Node], includeLinked bool, includeIndirectAncestors bool) list.List[*pkg.Node] {
 	if selectedNodes.Size() == 0 {
 		// Deal with this case separately for efficiency reasons
 		iterator := graph.FindMatchingNodes(input)
@@ -113,6 +136,106 @@ func collectDescendants(graph *pkg.Graph, node *pkg.Node, includeIndirect bool) 
 				descendant := graph.FindNodeByIndex(backlinkedIndex)
 				queue = append(queue, descendant)
 			}
+		}
+	}
+
+	return result
+}
+
+// collectDescendants2 collects the names of all backlinked nodes
+func collectDescendants2(graph *pkg.Graph, node *pkg.Node, includeIndirect bool) *set.IntSet {
+	result := set.NewIntSetWithInitialCapacity(graph.GetNodeCount())
+	queue := make([]*pkg.Node, 1, 20)
+	queue[0] = node
+
+	for len(queue) > 0 {
+		current := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+
+		for _, backlinked := range current.GetBacklinks() {
+			backlinkedIndex := backlinked.GetIndex()
+			result.Add(backlinkedIndex)
+
+			if includeIndirect {
+				descendant := graph.FindNodeByIndex(backlinkedIndex)
+				queue = append(queue, descendant)
+			}
+		}
+	}
+
+	return result
+}
+
+func collectDescendantIntersection(graph *pkg.Graph, nodes list.List[*pkg.Node], includeIndirect bool) *set.IntSet {
+	result := set.NewIntSetWithInitialCapacity(graph.GetNodeCount())
+
+	for i := range graph.GetNodeCount() {
+		result.Add(i)
+	}
+
+	for i := range nodes.Size() {
+		node := nodes.At(i)
+		r := collectDescendants2(graph, node, includeIndirect)
+		result.IntersectWith(r)
+	}
+
+	return result
+}
+
+func collectNodesMatchingSearch(graph *pkg.Graph, searchString string) *set.IntSet {
+	result := set.NewIntSetWithInitialCapacity(graph.GetNodeCount())
+	iterator := graph.FindMatchingNodes(searchString)
+
+	for iterator.Current() != nil {
+		result.Add(iterator.Current().GetIndex())
+		iterator.Next()
+	}
+
+	return result
+}
+
+func collectNodesMatchingSearches(graph *pkg.Graph, searchStrings []string) *set.IntSet {
+	result := set.NewIntSetWithInitialCapacity(graph.GetNodeCount())
+
+	for i := range graph.GetNodeCount() {
+		result.Add(i)
+	}
+
+	for _, searchString := range searchStrings {
+		r := collectNodesMatchingSearch(graph, searchString)
+		result.IntersectWith(r)
+	}
+
+	return result
+}
+
+func collectAncestors(graph *pkg.Graph, node *pkg.Node) *set.IntSet {
+	result := set.NewIntSetWithInitialCapacity(graph.GetNodeCount())
+	queue := queue.New[int]()
+	queue.Enqueue(node.GetIndex())
+
+	for !queue.IsEmpty() {
+		nodeID := queue.Dequeue()
+		if !result.Contains(nodeID) {
+			result.Add(nodeID)
+
+			node := graph.FindNodeByIndex(nodeID)
+			for _, parent := range node.GetLinks() {
+				queue.Enqueue(parent.GetIndex())
+			}
+		}
+	}
+
+	return result
+}
+
+func collectAncestorsUnion(graph *pkg.Graph, roots *set.IntSet) *set.IntSet {
+	result := set.NewIntSetWithInitialCapacity(graph.GetNodeCount())
+
+	for i := range graph.GetNodeCount() {
+		if roots.Contains(i) {
+			r := collectAncestors(graph, graph.FindNodeByIndex(i))
+			result.UnionWith(r)
 		}
 	}
 
