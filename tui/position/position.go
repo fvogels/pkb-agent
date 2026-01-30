740d39d9c92ce332811a3c018f55dc10e4b948e0
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-30 19:45:52 +0100

    Position.Move

--- tui/position/position.go
+++ tui/position/position.go
@@ -10,3 +10,10 @@ type Position struct {
 func (position Position) String() string {
 	return fmt.Sprintf("(%d, %d)", position.X, position.Y)
 }
+
+func (position Position) Move(dx int, dy int) Position {
+	return Position{
+		position.X + dx,
+		position.Y + dy,
+	}
+}
