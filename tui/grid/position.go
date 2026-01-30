97ea1e2eacf35499cd4889a469333f7cc6566a5a
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-30 19:48:05 +0100

    grid.Reposition

--- tui/grid/position.go
+++ tui/grid/position.go
@@ -3,6 +3,19 @@ package grid
 import "pkb-agent/tui/position"
 
 type positionedGrid struct {
-	grid   FiniteGrid
-	offset position.Position
+	grid Grid
+	dx   int
+	dy   int
+}
+
+func Reposition(grid Grid, dx int, dy int) Grid {
+	return &positionedGrid{
+		grid: grid,
+		dx:   dx,
+		dy:   dy,
+	}
+}
+
+func (grid *positionedGrid) At(position position.Position) Cell {
+	return grid.grid.At(position.Move(grid.dx, grid.dy))
 }
