9693d09fa2384196d3d79a300ca6d1bc17b4c1cb
Frederic Vogels <frederic.vogels@gmail.com>
2026-01-30 19:48:10 +0100

    grid.OverlayGrids

--- tui/grid/overlay.go
+++ tui/grid/overlay.go
@@ -2,23 +2,16 @@ package grid
 
 import (
 	"pkb-agent/tui/position"
-	"pkb-agent/tui/size"
 )
 
 type overlay struct {
-	size   size.Size
 	layers []FiniteGrid
 }
 
-func OverlayGrids(size size.Size, layers ...FiniteGrid) FiniteGrid {
+func OverlayGrids(layers ...FiniteGrid) Grid {
 	return &overlay{
-		size:   size,
 		layers: layers,
 	}
-}
-
-func (grid *overlay) Size() size.Size {
-	return grid.size
 }
 
 func (grid *overlay) At(pos position.Position) Cell {
@@ -31,8 +24,4 @@ func (grid *overlay) At(pos position.Position) Cell {
 	}
 
 	return Cell{}
-}
-
-func (grid *overlay) isValidPosition(position position.Position) bool {
-	return grid.Size().IsValidPosition(position)
 }
