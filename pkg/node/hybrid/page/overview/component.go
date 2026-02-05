5be52659367c17534c6001429d27336b9312d4c4
Frederic Vogels <frederic.vogels@gmail.com>
2026-02-05 21:59:22 +0100

    page overview shows page list

--- pkg/node/hybrid/page/overview/component.go
+++ pkg/node/hybrid/page/overview/component.go
@@ -1,14 +1,17 @@
 package overview
 
 import (
-	"fmt"
 	"pkb-agent/persistent/list"
 	"pkb-agent/pkg/node/hybrid/page"
 	"pkb-agent/tui"
-	"pkb-agent/tui/component/label"
+	"pkb-agent/tui/component/stringsview"
 	"pkb-agent/tui/data"
 	"pkb-agent/tui/grid"
+	"pkb-agent/util"
 	"pkb-agent/util/uid"
+
+	"github.com/gdamore/tcell/v3"
+	"github.com/gdamore/tcell/v3/color"
 )
 
 type pageComponent struct {
@@ -31,7 +34,18 @@ func NewPageComponent(messageQueue tui.MessageQueue, parent *Page) *pageComponen
 }
 
 func createRoot(messageQueue tui.MessageQueue, pages []page.Page) tui.Component {
-	return label.New(messageQueue, "label", data.NewConstant(fmt.Sprintf("%d pages", len(pages))))
+	// return label.New(messageQueue, "label", data.NewConstant(fmt.Sprintf("%d pages", len(pages))))
+
+	captionStyle := tcell.StyleDefault.Background(color.Reset).Foreground(color.Reset)
+	captionsAsSlice := util.Map(pages, func(page page.Page) stringsview.Item {
+		caption := page.GetCaption()
+		return stringsview.Item{
+			Runes: []rune(caption),
+			Style: &captionStyle,
+		}
+	})
+	captionsAsList := list.FromSlice(captionsAsSlice)
+	return stringsview.New(messageQueue, data.NewConstant(captionsAsList))
 }
 
 func (component *pageComponent) Render() grid.FiniteGrid {
